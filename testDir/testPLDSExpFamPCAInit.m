1c6d6fabd4a9fc9d4f15b955b5b6ef2b38aeead7
larsbuesing <lars@gatsby.ucl.ac.uk>
2014-01-15 05:32:11 +0000

    added test files

--- testDir/testPLDSExpFamPCAInit.m
+++ testDir/testPLDSExpFamPCAInit.m
@@ -5,28 +5,31 @@ close all
 xDim   = 5;
 yDim   = 100;
 T      = 100;
-Trials = 100;
+Trials = 50;
 
+
+%%%% generate data
 
 trueparams = PLDSgenerateExample('T',T,'Trials',Trials,'xDim',xDim,'yDim',yDim);
 trueparams.PiY = trueparams.C*dlyap(trueparams.A,trueparams.Q)*trueparams.C';
 seq = PLDSsample(trueparams,T,Trials);
 tp = trueparams;
 
-Y = [seq.y];
 
-clear params
-params.algorithmic.ExpFamPCA.lam = 1;
+%%% initialize with exponential family PCA
 
 params = PLDSInitialize(seq,xDim,'params',params,'initMethod','ExpFamPCA');
-
 subspace(tp.C,params.C)
+
+
+%%% do 10 EM iterations
 
 params.algorithmic.EMIterations.maxIter = 10;
 [params varBound] = PLDSEM(params,seq);
 subspace(tp.C,params.C)
 
 
+%%% plot some diagnostics
 
 %plotMatrixSpectrum(tp.A);
 %plotMatrixSpectrum(params.A);
@@ -40,10 +43,10 @@ figure
 plot(vec(tp.C*tp.A*tp.Pi*tp.C'),vec(params.C*params.A*params.Pi*params.C'),'xr')
 
 
-%figure
-%plot(vec(tp.C*tp.Q0*tp.C'),vec(params.C*params.Q*params.C'),'xr')
+figure
+plot(vec(tp.C*tp.Q0*tp.C'),vec(params.C*params.Q0*params.C'),'xr')
 
-%figure
-%plot(tp.C*tp.x0,params.C*params.x0,'xr')
+figure
+plot(tp.C*tp.x0,params.C*params.x0,'xr')
 
 
