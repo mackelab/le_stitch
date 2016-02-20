Major "bug-fix": the values for stationary covariance Pi are problematic.
Deleted all lines of code where the values for Pi as returned by ssdiSVD
were later on mixed by solutions of the Lyuapunov equation Pi = A*Pi*A'+Q,
as computed by dlyap.

Apparently, the heuristic applied in lack of a proper DARE solver is quite
often _very_ poor, and the Pi returned by ssidSVD indeed does not suffice the
Lyapunonv equation overly well.
Mixing the Pi's as returned by ssidSVD and those from dlyap in many cases
disrupts the iterative scheme.

The DARE solver imported from the control module currently still cannot be
used. It requires the FULL instantaneous covariance matrix to compute a
full-rank R. On the long run, a DARE solver for the general (non-stabilized)
case that assume diagonal R is needed!

iterSSID/python/stitching_ssid.py
@@ -30,7 +30,7 @@ def run_exp_iterSSID(seq, seq_f, n, num_iter=100,pi_method='proper', plot_flag=T
     SIGfp_f, SIGyy_f = generateCovariancesFP(seq=seq_f,hS=n)
 
     # fit model with iterative SSID
-    params, SIGfp_new, SIGyy_new, lPSIGfp, lPSIGyy = \
+    params, SIGfp_new, SIGyy_new, lPSIGfp, lPSIGyy, broken = \
         iterSSID(seq=seq, n=n, num_iter=num_iter, 
             pi_method=pi_method, init=None, alpha=1)
 
@@ -89,7 +89,7 @@ def run_exp_iterSSID(seq, seq_f, n, num_iter=100,pi_method='proper', plot_flag=T
         plt.ylabel('true')
         plt.show()    
 
-    return 
+    return params, SIGfp, SIGyy, lPSIGfp, lPSIGyy, SIGfp_f, SIGyy_f
 
 
 def iterSSID(seq, n, num_iter=100, init=None, alpha=1.,pi_method='proper'):
@@ -97,28 +97,16 @@ def iterSSID(seq, n, num_iter=100, init=None, alpha=1.,pi_method='proper'):
     T,p = seq['y'].shape
 
     params = []
+    broken = False
 
-    if len(seq['sub_pops']) == 1:
-        idx_overlap = seq['sub_pops'][0]
-    elif len(seq['sub_pops']) == 2:
-        idx_overlap = np.intersect1d(seq['sub_pops'][0], seq['sub_pops'][1])
-    else:
-        raise Exception('more than two subpopulations not implemented yet')
+    SIGfp_new, SIGyy_new = generateCovariancesFP(seq=seq, hS=n)
 
-    SIGfp, SIGyy = generateCovariancesFP(seq=seq, hS=n)
-
-    lnPSIGfp = np.isnan(SIGfp)    # logical versions, 
-    lnPSIGyy = np.isnan(SIGyy)    # readily usable 
-    lPSIGfp = np.invert(lnPSIGfp) # for indexing
-    lPSIGyy = np.invert(lnPSIGyy) #
-
-    PSIGfp  = SIGfp[lPSIGfp] # observed part of Hankel cov matrix
-    PSIGyy  = SIGyy[lPSIGyy] # observed part of cov matrix
+    lnPSIGfp, lnPSIGyy = np.isnan(SIGfp_new),np.isnan(SIGyy_new) # for indexing
+    lPSIGfp, lPSIGyy = np.invert(lnPSIGfp), np.invert(lnPSIGyy) 
+    PSIGfp, PSIGyy = SIGfp_new[lPSIGfp], SIGyy_new[lPSIGyy]  # observed parts
 
     # 'zero'-th iteration: used ssidSVD on Hankel cov matrix with NaN set to 0:
-    SIGfp_new = SIGfp
     SIGfp_new[lnPSIGfp] = 0
-    SIGyy_new = SIGyy
     SIGyy_new[lnPSIGyy] = 0
     params.append(post_process(ssidSVD(SIGfp=SIGfp_new,
                                        SIGyy=SIGyy_new,
@@ -135,34 +123,85 @@ def iterSSID(seq, n, num_iter=100, init=None, alpha=1.,pi_method='proper'):
         params.append(post_process(ssidSVD(SIGfp=SIGfp_old,
                                            SIGyy=SIGyy_old,
                                            n=n,
-                                           pi_method=pi_method)))
+                                           pi_method=pi_method,
+                                           params_old=params[-1])))
 
         SIGfp_new, SIGyy_new = construct_hankel_cov(params[-1])
 
-        SIGfp_new[lPSIGfp] = PSIGfp # make sure we keep the observed part right
+        if np.any(np.isnan(SIGfp_new)) or not np.all(np.isfinite(SIGfp_new)):
+            print('reconstructed Hankel cov matrix contains NaNs or Infs!')
+            print('cancelling iteration')
+
+            broken = True
+
+            break
+
+        # make sure we keep the observed part right
+        SIGfp_new[lPSIGfp], SIGyy_new[lPSIGyy] = PSIGfp, PSIGyy
         SIGfp_new[lnPSIGfp] = alpha*SIGfp_new[lnPSIGfp] \
                             +(1-alpha)*SIGfp_old[lnPSIGfp]
-        SIGyy_new[lPSIGyy] = PSIGyy # make sure we keep the observed part right
         SIGyy_new[lnPSIGyy] = alpha*SIGyy_new[lnPSIGyy] \
                             +(1-alpha)*SIGyy_old[lnPSIGyy]
-
         SIGyy_new = symmetrize(SIGyy_new)
 
-        SIGfp_old = SIGfp_new # attention: shallow copy!
-        SIGyy_old = SIGyy_new # attention: shallow copy!
 
-    return params, SIGfp_new, SIGyy_new, lPSIGfp, lPSIGyy
+        # hack: change halves of C if helpful
+        params[-1] = flip_emission_signs(SIGyy_new, seq['sub_pops'], params[-1])
+
+
+        SIGfp_old, SIGyy_old = SIGfp_new, SIGyy_new # shallow copies!
+
+
+    if broken:
+        broken = params[-1]
+        params.pop()
+        SIGfp_new, SIGyy_new = construct_hankel_cov(params[-1])
+        SIGfp_new[lPSIGfp], SIGyy_new[lPSIGyy] = PSIGfp, PSIGyy
+        SIGyy_new = symmetrize(SIGyy_new)
+
+    return params, SIGfp_new, SIGyy_new, lPSIGfp, lPSIGyy, broken
+
+
+def flip_emission_signs(SIGyy, sub_pops, params):
+
+    if len(sub_pops) == 1:
+        idx_overlap = sub_pops[0]
+    elif len(sub_pops) == 2:
+        idx_overlap = np.intersect1d(sub_pops[0], sub_pops[1])
+    else:
+        raise Exception('more than two subpopulations not yet implemented')
+
+    cov_h = params['C'][idx_overlap,:].dot(params['Pi']).dot(params['C'].T) + \
+        params['R'][idx_overlap,:]
+
+
+    for i in range(len(sub_pops)):
+        idx_sub = rem_overlap(sub_pops[i], idx_overlap)
+        SIGp_est, SIGp_true = cov_parts(cov_h,SIGyy,idx_sub,idx_overlap)
+
+        if check_flip(SIGp_est, SIGp_true):
+            params['C'][idx_sub,:] *= -1
+        
+    return params
+
+def check_flip(x_est, x_true, thresh=0.5):
+    return np.mean( np.sum(x_est * x_true, 1) < 0 ) > thresh
+
+def rem_overlap(idx_sub_pop, idx_overlap):
+    return np.setdiff1d(idx_sub_pop, idx_overlap)
+
+def cov_parts(SIG_est, SIG_true, idx_sub, idx_overlap):
+    return SIG_est[:,idx_sub],SIG_true[np.ix_(idx_overlap,idx_sub)]
 
 
 def construct_hankel_cov(params):
 
     p, hS = params['C'].shape
     SIGfp  = np.zeros((p*hS,p*hS))
-    Pi = dlyap(params['A'], params['Q'])
 
-    SIGyy = params['C'].dot(Pi.dot(params['C'].T)) + params['R']
+    SIGyy = params['C'].dot(params['Pi'].dot(params['C'].T)) + params['R']
 
-    covxx = Pi  
+    covxx = params['Pi']
     for k in range(2*hS-1):
         
         covxx =  params['A'].dot(covxx)
@@ -227,14 +266,15 @@ def generateCovariancesFP(seq,hS):
              
     return SIGfp, SIGyy
 
-def ssidSVD(SIGfp,SIGyy,n, pi_method='proper'):
+def ssidSVD(SIGfp,SIGyy,n, pi_method='proper', params_old = None):
     
     minVar    = 1e-5
     minVarPi  = 1e-5   
         
     p = np.size(SIGyy,0)
     
-    UU,SS,VV = np.linalg.svd(SIGfp)
+    UU,SS,VV = np.linalg.svd(SIGfp) # SIGfp = UU.dot(diag(SS).dot(VV)
+
     VV = VV.T
     SS = np.diag(SS[:n])
     UU = UU[:,:n]
@@ -245,6 +285,15 @@ def ssidSVD(SIGfp,SIGyy,n, pi_method='proper'):
     C = Obs[:p,:]
     Chat = VV[:p,:n]
     
+
+    # hack: abolish constant flipping of mathematical signs of latent states...
+    if not params_old is None:
+        flips = np.diag(2*(np.sum(C * params_old['C'],0) > 0).astype(np.float) - 1)
+        A = flips.dot(A.dot(flips))
+        C = C.dot(flips)
+        Chat = Chat.dot(flips)
+
+
     if pi_method=='proper':
         Pi,_,_ = control.matlab.dare(A=A.T,B=-C.T,Q=np.zeros((n,n)),R=-SIGyy,
             S=Chat.T, E=np.eye(n))    
@@ -286,8 +335,8 @@ def post_process(params):
         a,b = np.linalg.eig(params['Q'])
         params['Q'] = a*np.max(b,10**-10)*a.T
     
-    #if 'Q0' not in params:
-    #    params['Q0'] = np.real(control.dlyap(params['A'],params['Q']))
+    if 'Q0' not in params:
+        params['Q0'] = np.real(dlyap(params['A'],params['Q']))
     
     params.update({'x0':np.zeros(n)}) 
     params['R']  = np.diag(np.diag(params['R']))