99ea211ff9859bd6f31e1f3bf00188e0ff0b77d0
mnonnenm <mnonnenm@uos.de>
2017-01-20 14:50:39 +0100

    Fixing import of progress-bar range() object.

--- python/ssidid/__init__.py
+++ python/ssidid/__init__.py
@@ -1,4 +1,4 @@
-from .text import progprint
+from .text import progprint_xrange
 from .obs_scheme import ObservationScheme 
 from .utility import get_subpop_stats, gen_data
 from .SSID_Hankel_loss import run_bad
